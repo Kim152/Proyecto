a118ce0c25e2c3870422597acbc2462053cc3185
Kim <kimberly.sanchez@student.uclouvain.be>
2019-04-30 00:24:14 +0200

    Code

---
+++ ProyectoAlexa.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_ask import Ask, statement, question
+
+app = Flask(__name__)
+ask = Ask(app, '/')
+
+
+@ask.launch
+def start():
+    return question('Quel est votre nom ?')
+
+
+@ask.intent('HelloIntent')
+def bonjour(nom):
+    if nom is None:
+        return statement('Je ne connais pas ce nom, desole')
+    return question("Bonjour {} tu veux creer ton planing pour aujourd'hui ? ".format(nom))
+
+@ask.intent('AcesoIntent')
+def acceso(affirmacion):
+    if afirmacion == oui or afirmacion == Oui:
+        return question ('''Qu'est-ce que tu vas faire aujourd'hui ?''')
+
+@ask.intent('TareasIntent')
+def planing (tareas):
+    task = []
+    if tareas is None:
+        task.append(tareas)
+    return statement('''Les tâches pour aujourd'hui sont les suivantes ''')
+
+
+
+if __name__ == '__main__':
+    app.run()
